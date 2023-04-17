93771e692448a835a35fadd0d0a7e7d00609b877
Renzo San Juan <rcsanjuan96@gmail.com>
2023-04-17 05:06:28 -0700

    modified high score board to properly account for new and previously entered scores from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,17 +11,16 @@ var initialsEl = document.getElementById("initials");
 var highScoresEl = document.getElementById("highScores");
 var enterInitsButtonEl = document.getElementById("enterInits");
 var clearHighScoresEl = document.getElementById("clearHighScores");
-var playerData = {
-    name: "",
-    score: 0
-}
+
 
 //values
 var quizStatusCount = 0;
+const numOfChoices = 4;
 var secondsLeft = 30;
-var numOfAnswers = 4;
 var playerScore = 0;
 var timerRanOut = false;
+var playerData = [];
+
 
 var correctStr = "Correct";
 var wrongStr = "Wrong Answer";
@@ -36,7 +35,7 @@ var quizQuestions = [
     "Fill in what's missing in the following github workflow: make changes in local, git add, ______, git push origin [branch name]",
     //Question 5
     "CSS selectors can be used to target: "
-]
+];
 
 var quizAnswers = [
     //Question 1 Multiple Choices
@@ -62,19 +61,9 @@ var correctAnswers = [
     "git commit -m",
     //Question 5 Answer
     "all of the above"
-]
+];
 
-function renderNewQuestion() {
-    //display question problem in h1
-    questionEl.textContent = quizQuestions[quizStatusCount];
-    
-    //display question answers by creating new list items
-    for (var i = 0; i < numOfAnswers; i++) {
-        //Create answer list item
-        answerListEl.children[i].textContent = quizAnswers[quizStatusCount][i];
-    }
-}
-
+//initialize function
 function startQuiz(event) {
     event.preventDefault;
     //remove startQuiz button element
@@ -85,13 +74,22 @@ function startQuiz(event) {
     timerEl.textContent = "Timer: " + secondsLeft + "s";
     //start timer
     setTime();
-
-
 }
+
+function renderNewQuestion() {
+    //display question problem
+    questionEl.textContent = quizQuestions[quizStatusCount];
+    
+    //display question answers by creating new list items
+    for (var i = 0; i < numOfChoices; i++) {
+        answerListEl.children[i].textContent = quizAnswers[quizStatusCount][i];
+    }
+}
+
 // check if selected (clicked) answer is correct
 function checkAnswer(event) {
     let answer = event.target.textContent;
-    console.log(quizStatusCount);
+    //use quizStatusCount value to get correct answer index
     if (answer === correctAnswers[quizStatusCount]) {
         //show correct feedback
         feedbackEl.textContent = correctStr;
@@ -100,35 +98,27 @@ function checkAnswer(event) {
     } else {
         //show wrong feedback
         feedbackEl.textContent = wrongStr;
+        //deduct time
         secondsLeft -= 5;
     }
-
+    //update quiz status to be used as index for questions and answers array
     quizStatusCount++;
 
     //Check if end of questions is reached, if not go to next question
     if (quizStatusCount > 4)    {
         renderGameOver();
-    } else {
-        //Iterate quiz status to next set of questions, choices, and answer; 
+    } else { 
         renderNewQuestion();    
     }
-}
-// Stores player data (initials, score) to local storage
-function storePlayerData() {
-    enterInitsButtonEl.preventDefault;
-
-    var newPlayer = playerData;
-    newPlayer.name = initialsEl.value;
-    newPlayer.score = playerScore;
-    localStorage.setItem("PlayerData", JSON.stringify(newPlayer));
-
-    renderHighScores();
 }
 
 // Clears the High Score table values
 function clearHighScores() {
-    
     localStorage.clear();
+    clearHighScoreEls();
+    playerData = [];
+}
+function clearHighScoreEls() {
     while (highScoresEl.firstChild) {
         highScoresEl.removeChild(highScoresEl.firstChild);
     }
@@ -143,33 +133,46 @@ function renderGameOver() {
     else {
         questionEl.innerHTML = "Quiz End!";
     }
+    //remove answer choices
     while (answerListEl.firstChild) {
         answerListEl.removeChild(answerListEl.firstChild);
     }
-    //Display stats
-    timerEl.textContent = "Time Left: " + secondsLeft;
+
+    //Display plater stats
+    timerEl.textContent = "Time Left: " + secondsLeft + "s";
     playerScoreEl.innerHTML = "Score: " + playerScore;
     feedbackEl.innerHTML = "Thanks for playing";
     initialsEl.setAttribute("class", "");
     enterInitsButtonEl.setAttribute("class", "");
 
     renderHighScores();
-   
+}
+
+function storePlayerData() {
+    var newPlayerData = {
+        name: initialsEl.value.trim(),
+        score: playerScore
+    }
+    clearHighScoreEls();
+    playerData.unshift(newPlayerData);
+    localStorage.setItem("PlayerData", JSON.stringify(playerData));
+    renderHighScores();
 }
 
 //Display high score table
 function renderHighScores() {
     highScoresEl.setAttribute("class", "");
-    var newPlayerEl = document.createElement("h4");
-    var playerData = JSON.parse(localStorage.getItem("PlayerData"));
-
-    if (playerData === null)    {
-        newPlayerEl.textContent = "Name: " + " Score: ";
-    } else { 
-       newPlayerEl.textContent = "Name: " + playerData.name + " Score: " + playerData.score; 
+    clearHighScoresEl.setAttribute("class", ""); 
+    playerData = JSON.parse(localStorage.getItem("PlayerData"));
+    if (playerData === null) {
+        playerData = [];
+        return;
     }
-        highScoresEl.appendChild(newPlayerEl);
-        clearHighScoresEl.setAttribute("class", ""); 
+    for (var i = 0; i < playerData.length; i++) {   
+            var newPlayerEl = document.createElement("h4");
+            newPlayerEl.textContent = "Name: " + playerData[i].name + " Score: " + playerData[i].score; 
+            highScoresEl.appendChild(newPlayerEl);
+    }   
 }
 
 function setTime() {
@@ -177,21 +180,22 @@ function setTime() {
         secondsLeft--;
         timerEl.textContent = "Timer: " + secondsLeft + "s";
         
-        if (secondsLeft <= 0 || quizStatusCount > 4) {
+        if (secondsLeft <= 0) {
             clearInterval(timerInterval);
+        
             //reset secondsLeft to 0 if docked points go into the negatives
             if (secondsLeft < 0) {
                 secondsLeft = 0;
+                timerRanOut = true;
+                renderGameOver();
             }
-            timerRanOut = true;
-
-            renderGameOver();
-           // storePlayerData();
-        }
+        } else if (quizStatusCount > 4) {
+            clearInterval(timerInterval);
+            return;
+        }   
     }, 1000)
 }
-
 answerListEl.addEventListener("click", checkAnswer);
 startQuizButtonEl.addEventListener("click", startQuiz);
 enterInitsButtonEl.addEventListener("click", storePlayerData);
-clearHighScoresEl.addEventListener("click", clearHighScores)
+clearHighScoresEl.addEventListener("click", clearHighScores);
